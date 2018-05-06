c41fff50425c22e92aa5f817c4eb2483487acce6
Dcorum <dpcorum@yahoo.com>
2018-05-05 18:06:49 -0600

    updated code to include the basics of species working

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@
 // var catagories = ["films", "people", "planets", "species", "starships", "vehicles"]
 var test = 0;
 var used = [[], [], [], [], [], []];
+var answers = [[], [], [], [], [], []];
 var currentClass;
 // className = "films";
 function makeRequest(className, ident) {
@@ -36,11 +37,50 @@ function makeRequest(className, ident) {
 
 // makeRequest(className);
 
+
+// makeRequest(className);
+
 function getRandomNum(className) { //We need to know which numbers are broken
     var num;
+    var goodCheck = false;
     switch (className) {
         case "films":
             num = Math.floor(Math.random() * 7 + 1);
+            switch (num) {
+                case 1:
+                    num = 4;
+                    break;
+                case 2:
+                    num = 5;
+                    break;
+                case 3:
+                    num = 6;
+                    break;
+                case 4:
+                    num = 1;
+                    break;
+                case 5:
+                    num = 2;
+                    break;
+                case 6:
+                    num = 3;
+                    break;
+                case 7:
+                    num = 7;
+                    break;
+            }
+            do {
+
+                for (var i = 0; i < 7; i++) {
+                    if (used[0][i] === num) {
+                        num = Math.floor(Math.random() * 7 + 1);
+                        goodCheck = false;
+                        break;
+                    } else {
+                        goodCheck = true;
+                    }
+                }
+            } while (!goodCheck);
             break;
         case "people":
             num = Math.floor(Math.random() * 87 + 1);
@@ -52,7 +92,8 @@ function getRandomNum(className) { //We need to know which numbers are broken
             num = Math.floor(Math.random() * 61 + 1);
             break;
         case "species":
-            num = Math.floor(Math.random() * 37 + 1);
+            // num = Math.floor(Math.random() * 37 + 1);
+            num = Math.floor(Math.random() * 5 + 1);
             break;
         case "starships":
             num = Math.floor(Math.random() * 37 + 1);
@@ -61,218 +102,189 @@ function getRandomNum(className) { //We need to know which numbers are broken
             num = Math.floor(Math.random() * 39 + 1);
             break;
     }
-    // makeRequest(currentClass);
+    return num;
 
 }
-    // makeRequest(className);
-
-    function getRandomNum(className) { //We need to know which numbers are broken
-        var num;
-        var goodCheck = false;
-        switch (className) {
-            case "films":
-                num = Math.floor(Math.random() * 7 + 1);
-                    switch(num){
-                        case 1:
-                        num = 4;
-                        break;
-                        case 2:
-                        num = 5;
-                        break;
-                        case 3:
-                        num = 6;
-                        break;
-                        case 4:
-                        num = 1;
-                        break;
-                        case 5:
-                        num = 2;
-                        break;
-                        case 6:
-                        num = 3;
-                        break;
-                        case 7:
-                        num = 7;
-                        break;
-                    }
-                do {
-
-                    for (var i = 0; i < 7; i++) {
-                        if (used[0][i]===num) {
-                            num = Math.floor(Math.random() * 7 + 1);
-                            goodCheck = false;
-                            break;
-                        } else {
-                            goodCheck = true;
-                        }
-                    }
-                } while (!goodCheck);
-                break;
-            case "people":
-                num = Math.floor(Math.random() * 87 + 1);
-                if (num == 17) {
-                    num++;
-                }
-                break;
-            case "planets":
-                num = Math.floor(Math.random() * 61 + 1);
-                break;
-            case "species":
-                num = Math.floor(Math.random() * 37 + 1);
-                break;
-            case "starships":
-                num = Math.floor(Math.random() * 37 + 1);
-                break;
-            case "vehicles":
-                num = Math.floor(Math.random() * 39 + 1);
-                break;
-        }
-        return num;
-
-    }
-    var APIObject;
+var APIObject;
 
 
-    function APIHandler() {
-        var responseText = this.responseText;
-        // console.log("URL response is: "+responseText);
-        APIObject = JSON.parse(responseText);
-        // console.log(APIObject.length);
-        console.log()
-        LoadAPI(APIObject);
+function APIHandler() {
+    var responseText = this.responseText;
+    // console.log("URL response is: "+responseText);
+    APIObject = JSON.parse(responseText);
+    // console.log(APIObject.length);
+    console.log()
+    LoadAPI(APIObject);
 
-    }
+}
 
-    function LoadAPI(APIObject) {
-        var objectContainer = document.getElementById("objectContainer");
-        console.log(APIObject);
+function LoadAPI(APIObject) {
+    var objectContainer = document.getElementById("objectContainer");
+    console.log(APIObject);
 
 
 
 
-        var question;
-        switch (currentClass) {
-            case "films":
-                var goodCheck = false;
-                var id = APIObject.episode_id;
-                switch(id){
-                    case 1:
+    var question;
+    switch (currentClass) {
+        case "films":
+            var goodCheck = false;
+            var id = APIObject.episode_id;
+            switch (id) {
+                case 1:
                     id = 4;
                     break;
-                    case 2:
+                case 2:
                     id = 5;
                     break;
-                    case 3:
+                case 3:
                     id = 6;
                     break;
-                    case 4:
+                case 4:
                     id = 1;
                     break;
-                    case 5:
+                case 5:
                     id = 2;
                     break;
-                    case 6:
+                case 6:
                     id = 3;
                     break;
-                    case 7:
+                case 7:
                     id = 7;
                     break;
-                }
-                switch (id) {
-                    //random questions inside nested switch based on currentclass
-                    case 1:
-                        used[0][0] = id;
-                        question = APIObject.opening_crawl;
-                        break;
-                    case 2:
-                        used[0][1] = id;
-                        question = APIObject.opening_crawl;
-                        break;
-                    case 3:
-                        used[0][2] = id;
-                        question = APIObject.opening_crawl;
-                        break;
-                    case 4:
-                        used[0][3] = id;
-                        question = APIObject.opening_crawl;
-                        break;
-                    case 5:
-                        used[0][4] = id;
-                        question = APIObject.opening_crawl;
-                        break;
-                    case 6:
-                        used[0][5] = id;
-                        question = APIObject.opening_crawl;
-                        break;
-                    case 7:
-                        used[0][6] = id;
-                        question = APIObject.opening_crawl;
-                        break;
+            }
+            switch (id) {
+                //random questions inside nested switch based on currentclass
+                case 1:
+                    used[0][0] = id;
+                    answers[0][0] = APIObject.title;
+                    question = APIObject.opening_crawl;
+                    break;
+                case 2:
+                    used[0][1] = id;
+                    answers[0][1] = APIObject.title
+                    question = APIObject.opening_crawl;
+                    break;
+                case 3:
+                    used[0][2] = id;
+                    answers[0][2] = APIObject.title
+                    question = APIObject.opening_crawl;
+                    break;
+                case 4:
+                    used[0][3] = id;
+                    answers[0][3] = APIObject.title
+                    question = APIObject.opening_crawl;
+                    break;
+                case 5:
+                    used[0][4] = id;
+                    used[0][4] = APIObject.title
+                    question = APIObject.opening_crawl;
+                    break;
+                case 6:
+                    used[0][5] = id;
+                    answers[0][5] = APIObject.title
+                    question = APIObject.opening_crawl;
+                    break;
+                case 7:
+                    used[0][6] = id;
+                    answers[0][6] = APIObject.title
+                    question = APIObject.opening_crawl;
+                    break;
 
-                }
-                break;
-            case "people":
-                var num = Math.floor(Math.random());
-                switch (num) {
-                    case 1:
+            }
+            break;
+        case "people":
+            var num = Math.floor(Math.random());
+            switch (num) {
+                case 1:
 
-                        break;
+                    break;
 
-                    case 2:
+                case 2:
 
-                        break;
+                    break;
 
-                    case 3:
+                case 3:
 
-                        break;
+                    break;
 
-                    case 4:
+                case 4:
 
-                        break;
+                    break;
 
-                    case 5:
+                case 5:
 
-                        break;
-
-
+                    break;
 
 
-                }
-                break;
-            case "planets":
-                switch (num) {
 
-                }
-                break;
-            case "species":
-                switch (num) {
 
-                }
-                break;
-            case "starships":
-                switch (num) {
+            }
+            break;
+        case "planets":
+            switch (num) {
 
-                }
-                break;
-            case "vehicles":
-                switch (num) {
+            }
+            break;
+        case "species":
+            switch (APIObject.name) {
+                case "Human":
+                    used[2][0] = APIObject.name;
+                    answers[2][0] = APIObject.name;
+                    question = "This species is classified as a mammal and lives an average life of 120 earth years.";
+                    break;
 
-                }
-                break;
+                case "Droid":
+                    used[2][0] = APIObject.name;
+                    answers[2][0] = APIObject.name;
+                    question = "This species is classified as a artificial robot and has an indefinite life span(Debatable).";
+                    break;
 
-        }
+                case "Wookiee":
+                    used[2][0] = APIObject.name;
+                    answers[2][0] = APIObject.name;
+                    question = "This species is classified as a mammal and lives an average life of 400 earth years.";
+                    break;
 
-        var newDiv = document.createElement("div");
-        var newSpan = document.createElement("span");
+                case "Rodian":
+                    used[2][0] = APIObject.name;
+                    answers[2][0] = APIObject.name;
+                    question = "This species is classified as a sentient reptile and has an unknown average life span.";
+                    break;
 
-        newSpan.textContent = question;
+                case "Hutt":
+                    used[2][0] = APIObject.name;
+                    answers[2][0] = APIObject.name;
+                    question = "This species is classified as a gastropod and lives an average life of 1000 earth years.";
+                    break;
 
-        newDiv.appendChild(newSpan);
-        objectContainer.appendChild(newDiv);
+
+            }
+            break;
+        case "starships":
+            switch (num) {
+
+            }
+            break;
+        case "vehicles":
+            switch (num) {
+
+            }
+            break;
 
     }
 
-    var userForm = document.getElementById("usernameForm");
+    var newDiv = document.createElement("div");
+    var newSpan = document.createElement("span");
+
+    newSpan.textContent = question;
+
+    newDiv.appendChild(newSpan);
+    objectContainer.appendChild(newDiv);
+
+}
+
+var userForm = document.getElementById("usernameForm");
 
     // userForm.onsubmit = function(){
 
